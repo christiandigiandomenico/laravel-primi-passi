62196507d4d429394d1e16bb9c736afcb4c35131
christiandigiandomenico <christian.digiandomenico1990@gmail.com>
2024-04-05 16:54:15 +0200

    Added links

--- resources/views/seconda.blade.php
+++ resources/views/seconda.blade.php
@@ -15,8 +15,7 @@
       <h5 class="card-title">Hello World 2</h5>
       <h6 class="card-subtitle mb-2 text-body-secondary">{{$saluto}}</h6>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      <a href="{{route('home')}}" class="card-link">Go back</a>
     </div>
   </div>
 
